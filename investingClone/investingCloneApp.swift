4e39d2dae361cd78cd87218a9229419eb51b831a
AMG <alimertgulec38@gmail.com>
2022-11-29 16:03:46 +0300

    Initial Commit

---
+++ investingClone/investingCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  investingCloneApp.swift
+//  investingClone
+//
+//  Created by Ali Mert Güleç on 29.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct investingCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
