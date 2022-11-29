7989eb21f9ab0a3aaebf74e9b2cf39a6ea6da1d1
AMG <alimertgulec38@gmail.com>
2022-11-29 16:13:34 +0300

    view group and login screen added

---
+++ investingClone/VIEWS/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  investingClone
+//
+//  Created by Ali Mert Güleç on 29.11.2022.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Welcome to the login screen")
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
