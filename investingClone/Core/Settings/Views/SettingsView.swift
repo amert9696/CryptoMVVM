2f6308b5288ff41dfa91de033ffb41541a87bdc5
AMG <alimertgulec38@gmail.com>
2023-01-03 13:16:52 +0300

    sth changed

--- investingClone/Core/Settings/Views/SettingsView.swift
+++ investingClone/Core/Settings/Views/SettingsView.swift
@@ -6,17 +6,17 @@
 //
 
 import SwiftUI
-
+import SafariServices
 
 //EVERY DETAIL WILL BE CHANGE --- !!!!!!!
 
 struct SettingsView: View {
     
     let defaultURL = URL(string: "https://www.google.com")!
-    let youtubeURL = URL(string: "https://www.youtube.com")!
-    let coffeeURL = URL(string: "https://www.buymecoffee.com")!
     let coingeckoURL = URL(string: "https://www.coingecko.com")!
-    let personelURL = URL(string: "https://www.github/amert9696.com")!
+    
+  
+    
     
     var body: some View {
         NavigationView{
@@ -28,13 +28,13 @@ struct SettingsView: View {
                 
                 //content
                 List{
-                    sections
-                        .listRowBackground(Color.theme.background.opacity(0.5))
-                    coinGeckoSection
-                        .listRowBackground(Color.theme.background.opacity(0.5))
+//                    generalAppInfoPart
+//                        .listRowBackground(Color.theme.background.opacity(0.5))
                     developerSection
                         .listRowBackground(Color.theme.background.opacity(0.5))
-                    applicationSection
+                    coinGeckoSection
+//                        .listRowBackground(Color.theme.background.opacity(0.5))
+//                    applicationSection
                         .listRowBackground(Color.theme.background.opacity(0.5))
                 }
             }
@@ -42,7 +42,7 @@ struct SettingsView: View {
             .font(.headline)
             .accentColor(.blue)
             .listStyle(GroupedListStyle())
-            .navigationTitle("Settings")
+            .navigationTitle("About me and app")
             .toolbar{
                 ToolbarItem(placement: .navigationBarLeading){
                     XMarkButton()
@@ -57,26 +57,28 @@ struct SettingsView_Previews: PreviewProvider {
         SettingsView()
     }
 }
- 
+
 extension SettingsView {
-    
-    private var sections: some View {
-        Section(header: Text("SWIFTUI")){
-            
-            VStack(alignment: .leading){
-                Image("logo")
-                    .resizable()
-                    .frame(width: 100,height: 100)
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                Text("This app was made by me. It uses MVVM Architecture,Combine and CoreData!")
-                    .font(.callout)
-                    .fontWeight(.medium)
-                    .foregroundColor(Color.theme.accent)
-            }.padding(.vertical)
-            Link("Go to youtube",destination: youtubeURL)
-            Link("Coffee website ",destination: coffeeURL)
-        }
-    }
+
+//    private var generalAppInfoPart: some View { // will be update
+//        Section(header: Text("SWIFTUI")){
+//
+//            VStack(alignment: .leading){
+//                Image("logo")
+//                    .resizable()
+//                    .frame(width: 100,height: 100)
+//                    .clipShape(RoundedRectangle(cornerRadius: 20))
+//                Text("This app was made by me. It uses MVVM Architecture,Combine and CoreData!")
+//                    .font(.callout)
+//                    .fontWeight(.medium)
+//                    .foregroundColor(Color.theme.accent)
+//            }.padding(.vertical)
+//            Link("You can go to my github ",destination: githubURL)
+//            Link("Follow me on twitter ",destination: twitterURL)
+//            Link("My linkedIn account ",destination: linkedInURL)
+//
+//        }
+//    }
     
     private var coinGeckoSection: some View {
         Section(header: Text("CoinGecko")){
@@ -98,34 +100,102 @@ extension SettingsView {
     }
     
     private var developerSection: some View {
-        Section(header: Text("Developer")){
+        Section(header: Text("Developer Information")){
             
-            VStack(alignment: .leading){
-                Image("me")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 100)
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                Text("I am an iOS developer and 3rd year Computer Engineering Student. The project benefits from multi-threading,publishers/subscribers, and data persistance.")
-                    .font(.callout)
-                    .fontWeight(.medium)
-                    .foregroundColor(Color.theme.accent)
-            }.padding(.vertical)
-            Link("Go to my github account",destination: personelURL)
-            
+            VStack(alignment:.leading){
+                VStack(alignment: .leading){
+                    Image("me")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 100)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                    Text("I am an iOS developer and 3rd year Computer Engineering Student. The project benefits from multi-threading,publishers/subscribers, and data persistance.If you want to contact me, you can reach the following accounts.")
+                        .font(.callout)
+                        .fontWeight(.medium)
+                        .foregroundColor(Color.theme.accent)
+                }.padding(.vertical)
+                 
+                    }
+            githubAccountButton
+            twitterAccountButton
+            mediumButton
+            linkedInButton
+              
+                        }
+                    }
         }
-    }
+//    private var applicationSection: some View {
+//        Section(header: Text("Developer")){
+//            Link("Terms of Service",destination: defaultURL)
+//            //Link("Privacy Policy",destination: defaultURL)  -> UPDATE THIS LINKS
+//            //Link("Compony Website",destination: defaultURL)
+//            //Link("Learn More",destination: defaultURL)
+//
+//        }
+//    } }
+
+private var githubAccountButton: some View{
+    Button(action: {
+                    guard let url = URL(string:"https://github.com/amert9696") else { return }
+                    UIApplication.shared.open(url)
+                }) {
+                    HStack{
+                        Text("Go to my GitHub account")
+                        Image("github-mark")
+                                            .resizable()
+                                            .frame(width: 30,height: 30)
+                                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                    }
+                }
     
-    private var applicationSection: some View {
-        Section(header: Text("Developer")){
-            Link("Terms of Service",destination: defaultURL)
-            //Link("Privacy Policy",destination: defaultURL)  -> UPDATE THIS LINKS
-            //Link("Compony Website",destination: defaultURL)
-            //Link("Learn More",destination: defaultURL)
+}
+private var twitterAccountButton: some View {
+    
+    Button(action: {
+        guard let url = URL(string: "https://twitter.com/alimertgulec") else { return }
+        UIApplication.shared.open(url)
+    }) {
+        HStack{
+            Text("Go to my Twitter account")
+            Image("twitter")
+                                .resizable()
+                                .frame(width: 30,height: 30)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
             
         }
     }
 }
+
+private var linkedInButton : some View {
     
+    Button(action: {
+        guard let url = URL(string: "https://www.linkedin.com/in/ali-mert-g%C3%BCle%C3%A7-600908196") else { return }
+        UIApplication.shared.open(url)
+    }) {
+        HStack{
+            Text("Go to my LinkedIn account")
+            Image("linked-in")
+                                .resizable()
+                                .frame(width: 30,height: 30)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+            
+        }
+    }
+}
+private var mediumButton: some View {
+    Button(action: {
+        guard let url = URL(string: "https://medium.com/@amert96") else { return }
+        UIApplication.shared.open(url)
+    }) {
+        HStack{
+            Text("Go to my medium account")
+            Image("medium")
+                .resizable()
+                .frame(width: 30,height: 30)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            
+        }
+    }
     
-    
+}
+
